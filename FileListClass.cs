feed8bb02ea94d579a1ea54c9ea70b5cbc983e23
ishiizumi <rrmkk086@yahoo.co.jp>
2022-08-17 12:34:42 +0900

    2022/8/18 wfolder -> WorkFolder 変数名変更

--- FileListClass.cs
+++ FileListClass.cs
@@ -37,8 +37,10 @@ namespace FileListLibrary
         /// <summary>
         /// 引数のListから比較対象外のファイルを削除する
         /// 設定ファイル exclude.txt
+        /// <param name="WorkFolder">ワーク用フォルダ</param>
+        /// <param name="folderlist"/>指定されたフォルダ内のファイル一覧List</param>
         /// </summary>
-        public List<string> GetReadyList(string wfolder,  List<String> folderlist)
+        public List<string> GetReadyList(string WorkFolder,  List<String> folderlist)
         {
             if (!(folderlist?.Count > 0))
             {
@@ -47,7 +49,7 @@ namespace FileListLibrary
             List<string> extension = new List<string>();//管理しない拡張子
             List<string> unnecessary = new List<string>();//管理しないファイル
 
-            string FileName = Path.Combine(wfolder, excludefilename);//除外設定ファイル
+            string FileName = Path.Combine(WorkFolder, excludefilename);//除外設定ファイル
             if (File.Exists(FileName))
             {
                 IEnumerable<string> lines = File.ReadLines(FileName, SJIS);
@@ -82,14 +84,14 @@ namespace FileListLibrary
         /// <summary>
         /// 実行フォルダに有るファイルをWorkフォルダへコピーする
         /// </summary>
-        /// <param name="wfolder"></param>コピー先フォルダ名
+        /// <param name="WorkFolder"></param>コピー先フォルダ名
         /// <param name="fname"></param>コピー元ファイル名
-        public void fileCopy(string wfolder, string fname)
+        public void fileCopy(string WorkFolder, string fname)
         {
             string fromfname = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fname);//実行フォルダ
-            if (Directory.Exists(wfolder) & File.Exists(fromfname))
+            if (Directory.Exists(WorkFolder) & File.Exists(fromfname))
             {
-                File.Copy(fromfname, Path.Combine(wfolder, fname), true);
+                File.Copy(fromfname, Path.Combine(WorkFolder, fname), true);
             }
         }
 
@@ -120,12 +122,12 @@ namespace FileListLibrary
         /// copyfile内のファイルをコピー先のフォルダにコピーする
         /// </summary>
         /// <param name="copyfilelist"></param>コピー対象List
-        /// <param name="wfolder"></param>コピー先フォルダ名
+        /// <param name="WorkFolder"></param>コピー先フォルダ名
         /// <param name="Before"></param>作業前・作業後の判断
-        public void FolderCopy(List<FileSetdata.FileSetdata> copyfilelist, string wfolder, Boolean before)
+        public void FolderCopy(List<FileSetdata.FileSetdata> copyfilelist, string WorkFolder, Boolean before)
         {
             string lastfoldername = GetLastFolderName(setfoldername);
-            string workfoldernamer = wfolder + (before ? beforeFolder : afterFolder);
+            string workfoldernamer = WorkFolder + (before ? beforeFolder : afterFolder);
             Directory.CreateDirectory(workfoldernamer);
             foreach (var sdata in copyfilelist)
             {
@@ -144,16 +146,16 @@ namespace FileListLibrary
         /// </summary>
         /// <param name="wfoldr"></param> WorkFolder
         /// /// <param name="filesetdatas"></param>ファイル一覧List
-        public List<FileSetdata.FileSetdata> CopyFileListCreate(string wfolder, List<FileSetdata.FileSetdata> filesetdatas)
+        public List<FileSetdata.FileSetdata> CopyFileListCreate(string WorkFolder, List<FileSetdata.FileSetdata> filesetdatas)
         {
             var copyfilelist = new List<FileSetdata.FileSetdata>();//コピー対象ファイル
 
-            if (!File.Exists(Path.Combine(workfolder, selectfilename)))
+            if (!File.Exists(Path.Combine(WorkFolder, selectfilename)))
             {
                 return copyfilelist; ;
             }
 
-            foreach (var selectdata in File.ReadLines(Path.Combine(wfolder, selectfilename), SJIS))
+            foreach (var selectdata in File.ReadLines(Path.Combine(WorkFolder, selectfilename), SJIS))
             {
                 foreach (var sdata in filesetdatas.Where(c => Path.Combine(c.FolderName, c.FileName).Substring(1) == selectdata))
                 {
